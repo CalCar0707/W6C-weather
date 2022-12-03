4e8a3c1f98e365fd7143eee9d85109d82d256354
Call Carl <callienicole0419@gmail.com>
2022-12-03 16:36:41 -0600

    added Api and key access

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+//variables
+var apiKey = '5cf9dab34a270b7bc76596bb2d6c4d94';
+var city = '';
+
+var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
+
+fetch(queryUrl);
