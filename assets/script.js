eeac2f6b346e72f8086d9c3699f9b9e183489801
Call Carl <callienicole0419@gmail.com>
2022-12-03 21:54:14 -0600

    added todays date w/dayjs

--- assets/script.js
+++ assets/script.js
@@ -3,11 +3,18 @@ var apiKey = '5cf9dab34a270b7bc76596bb2d6c4d94';
 var city = '';
 var today = dayjs();
 
+//search for city 
+var searchBtn = document.getElementById('city-search');
+
+
 var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
 
 
-fetch(queryUrl);
-//todays info inccluding location, date, temp, wind, humidity
 
+fetch(queryUrl);
+//todays info including location, date, temp, wind, humidity, wind speed
+
+var todaysDate = dayjs();
+$('#date').text(today.format('M/D/YYYY'));
 
 //5 day forecast
