a585dfd876e1891d082e7107fca909fa183d72a1
Call Carl <callienicole0419@gmail.com>
2022-12-03 21:05:06 -0600

    added jquery and day js links

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,13 @@
 //variables
 var apiKey = '5cf9dab34a270b7bc76596bb2d6c4d94';
 var city = '';
+var today = dayjs();
 
 var queryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
 
 
 fetch(queryUrl);
+//todays info inccluding location, date, temp, wind, humidity
+
+
+//5 day forecast
