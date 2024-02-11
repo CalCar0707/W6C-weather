b6deaa8e01a698c0a9dc3c59c5b25e356fd1da9e
CalCarl <callienicole0419@gmail.com>
2024-02-10 18:26:23 -0600

    updated where city name is not in for loop and date is showing

--- assets/script.js
+++ assets/script.js
@@ -17,7 +17,8 @@ var searchBtn = document.getElementById('city-search');
 //function to fetch todays weather data- WORKING PROPERLY
 function todaysWeather() {
     queryUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city.value + '&appid=' + apiKey;
-    
+    console.log(city.value);
+
     fetch(queryUrl)
         .then(function (response) {
          return response.json();
@@ -30,8 +31,8 @@ function todaysWeather() {
             listItem.textContent = data[i].name;
             currentWeather.appendChild(listItem);
         }
-        //todays weather data-WORKING
-        //todays info including location, date, temp, wind, humidity, wind speed
+        //todays weather data
+                //todays info including location, date, temp, wind, humidity, wind speed
         var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + data[0].lat + '&lon=' + data[0].lon + '&appid=' + apiKey + '&units=imperial';
         fetch(weatherUrl)
         .then(function (response) {
@@ -41,17 +42,22 @@ function todaysWeather() {
         .then(function (data) {
             console.log(data);
             
+            currentWeather.append(city.value);
+
             //for loop for 5 day forecast- WORKING
             for (var i = 0; i < data.list.length; i=i+8) {
-            var temperature = document.createElement('h3');
-            var windSpeed = document.createElement('h4');
-            var humidity = document.createElement('h4');
+                var date = document.createElement('h2');
+                var temperature = document.createElement('h3');
+                var windSpeed = document.createElement('h4');
+                var humidity = document.createElement('h4');
 
+            date.textContent = data.list[i].dt_txt;
             temperature.textContent = 'Temp: ' + data.list[i].main.temp + '° F';
             windSpeed.textContent = 'Wind: ' + data.list[i].wind.speed + ' MPH';
             humidity.textContent = 'Humidity: ' + data.list[i].main.humidity + '%';
 
-            currentWeather.append(city.value);
+            //dont need city to show several times in loop
+            currentWeather.append(date);
             currentWeather.append(temperature);
             currentWeather.append(windSpeed);
             currentWeather.append(humidity);
