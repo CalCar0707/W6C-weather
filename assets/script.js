9af5b6fb4c9143e01b888fab1f59199f5f28f49a
CalCarl <callienicole0419@gmail.com>
2024-04-03 09:43:30 -0500

    fixed previous city search to show on page

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,7 @@
 //global variables
-
-// user input city being searched
-var city = document.getElementById('city-input');
-
-//todays weather div display
+var cityInput = document.querySelector('.city-input');
+var searchButton = document.querySelector('.search-btn');
+//todays weather display
 var currentWeather = document.getElementById('todays-weather');
 
 //previous search history
@@ -11,19 +9,16 @@ var prevCityList = document.getElementById('prev-city-list');
 
 //openweather api variables
 var apiKey = '5cf9dab34a270b7bc76596bb2d6c4d94';
-var queryUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city.value + '&appid=' + apiKey;
+var queryUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput.value + '&appid=' + apiKey;
 //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 //dayjs variables
 var today = dayjs();
 
-//search button variable
-var searchBtn = document.getElementById('search-btn');
-
 
 //function to fetch todays weather data- WORKING PROPERLY
 function todaysWeather() {
-    queryUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city.value + '&appid=' + apiKey;
+    queryUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput.value + '&appid=' + apiKey;
 
     fetch(queryUrl)
         .then(function (response) {
@@ -33,10 +28,10 @@ function todaysWeather() {
     .then(function (data) {
         console.log(data);
         for (var i = 0; i < data.length; i++) {
-            var prevCitySearch = document.createElement('h2');
+            var prevCitySearch = document.createElement('button');
             prevCitySearch.textContent = data[i].name;
             console.log(prevCitySearch);
-            currentWeather.appendChild(prevCitySearch);
+            prevCityList.appendChild(prevCitySearch);
         }
         //todays weather data
                 //todays info including location, date, temp, wind, humidity, wind speed
@@ -54,23 +49,23 @@ function todaysWeather() {
             for (var i = 0; i < data.list.length; i=i+8) {
                 var date = document.createElement('h3');
                 var temperature = document.createElement('h4');
-                var weather = document.createElement('div');
-                var icon = document.createElement('img');
-                var windSpeed = document.createElement('div');
-                var humidity = document.createElement('div');
+                // var icon = document.createElement('img');
+                var windSpeed = document.createElement('h4');
+                var humidity = document.createElement('h4');
+                var weather = document.createElement('h4');
 
             date.textContent = data.list[i].dt_txt;
             temperature.textContent = 'Temp: ' + data.list[i].main.temp + '° F';
-            weather.textContent = data.list[i].weather[0].main 
-            icon.textContent = data.list[i].weather[0].icon
+            // icon.textContent = data.list[i].weather[0].icon
             windSpeed.textContent = 'Wind: ' + data.list[i].wind.speed + ' MPH';
             humidity.textContent = 'Humidity: ' + data.list[i].main.humidity + '%';
+            weather.textContent = data.list[i].weather[0].main 
 
             // date.textContent = data.list[0].dt_txt;
             // temperature.textContent = 'Temp: ' + data.list[0].main.temp + '° F';
             // windSpeed.textContent = 'Wind: ' + data.list[0].wind.speed + ' MPH';
             // humidity.textContent = 'Humidity: ' + data.list[0].main.humidity + '%';
-            console.log(icon);
+            // console.log(icon);
 
             // if(icon === '04d') {
             //     var icon = document.createElement('img');
@@ -86,7 +81,8 @@ function todaysWeather() {
             currentWeather.append(humidity);
             }
             })
-       })
+       }
+       )
     }
   //  );
     
@@ -94,7 +90,7 @@ function todaysWeather() {
 //}
 
 //local storage
-localStorage.setItem('city', JSON.stringify((city.value)));
+localStorage.setItem('city', JSON.stringify((cityInput.value)));
 localStorage.getItem('city');
 
 //var prevCity = localStorage.getItem(city.value);
@@ -106,4 +102,4 @@ var todaysDate = dayjs();
 //$('#date').text(today.format('M/D/YYYY'));
 
 //5 day forecast
-searchBtn.addEventListener('click', todaysWeather);
+searchButton.addEventListener('click', todaysWeather);
