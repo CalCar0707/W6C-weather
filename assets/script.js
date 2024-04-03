621bb2f646be3d1807483ec59ddcea397ecb0cb4
CalCarl <callienicole0419@gmail.com>
2024-04-03 10:37:44 -0500

    updated variables to use let instead of var per es6 syntax

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,23 @@
 //global variables
-var cityInput = document.querySelector('.city-input');
-var searchButton = document.querySelector('.search-btn');
+let cityInput = document.querySelector('.city-input');
+let searchButton = document.querySelector('.search-btn');
 //todays weather display
-var currentWeather = document.getElementById('todays-weather');
+let currentWeather = document.getElementById('todays-weather');
+let weatherCardsDiv = document.querySelector('.weather-cards');
+// variables for weather cards 
+let date = document.querySelector('.date'); 
+
 
 //previous search history
-var prevCityList = document.getElementById('prev-city-list');
+let prevCityList = document.getElementById('prev-city-list');
 
 //openweather api variables
-var apiKey = '5cf9dab34a270b7bc76596bb2d6c4d94';
-var queryUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput.value + '&appid=' + apiKey;
+let apiKey = '5cf9dab34a270b7bc76596bb2d6c4d94';
+let queryUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + cityInput.value + '&appid=' + apiKey;
 //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 //dayjs variables
-var today = dayjs();
+let today = dayjs();
 
 
 //function to fetch todays weather data- WORKING PROPERLY
@@ -27,15 +31,15 @@ function todaysWeather() {
 //adds prev search history to list
     .then(function (data) {
         console.log(data);
-        for (var i = 0; i < data.length; i++) {
-            var prevCitySearch = document.createElement('button');
+        for (let i = 0; i < data.length; i++) {
+            let prevCitySearch = document.createElement('h2');
             prevCitySearch.textContent = data[i].name;
             console.log(prevCitySearch);
             prevCityList.appendChild(prevCitySearch);
         }
         //todays weather data
                 //todays info including location, date, temp, wind, humidity, wind speed
-        var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + data[0].lat + '&lon=' + data[0].lon + '&appid=' + apiKey + '&units=imperial';
+        let weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + data[0].lat + '&lon=' + data[0].lon + '&appid=' + apiKey + '&units=imperial';
         fetch(weatherUrl)
         .then(function (response) {
             return response.json();
@@ -46,39 +50,42 @@ function todaysWeather() {
             
 
             //for loop for 5 day forecast- WORKING
-            for (var i = 0; i < data.list.length; i=i+8) {
-                var date = document.createElement('h3');
-                var temperature = document.createElement('h4');
-                // var icon = document.createElement('img');
-                var windSpeed = document.createElement('h4');
-                var humidity = document.createElement('h4');
-                var weather = document.createElement('h4');
+            for (let i = 0; i < data.list.length; i=i+8) {
+                console.log(data.list[i].dt_txt.split(' ')[0]);
+                console.log(data.list[i].weather[0].icon);
+                console.log(data.list[i].main.temp);
+                console.log(data.list[i].wind.speed);
+                console.log(data.list[i].main.humidity);
+                    // return `<li class="card">
+                    //     <h3>( ${data.list[i].dt_txt.split(' ')[0]} )</h3>
+                    //     <img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@4x.png" alt="weather-icon">
+                    //     <h4>Temp: ${data.list[i].main.temp}° F</h4>
+                    //     <h4>Wind: ${data.list[i].wind.speed} MPH</h4>
+                    //     <h4>Humidity: ${data.list[i].main.humidity}%</h4>
+                    // </li>`
 
-            date.textContent = data.list[i].dt_txt;
-            temperature.textContent = 'Temp: ' + data.list[i].main.temp + '° F';
+
+            //     var date = document.createElement('h3');
+            //     var temperature = document.createElement('h4');
+            //     // var icon = document.createElement('img');
+            //     var windSpeed = document.createElement('h4');
+            //     var humidity = document.createElement('h4');
+            //     var weather = document.createElement('h4');
+
+             date.textContent = data.list[i].dt_txt.split(' ')[0];
+            // temperature.textContent = 'Temp: ' + data.list[i].main.temp + '° F';
             // icon.textContent = data.list[i].weather[0].icon
-            windSpeed.textContent = 'Wind: ' + data.list[i].wind.speed + ' MPH';
-            humidity.textContent = 'Humidity: ' + data.list[i].main.humidity + '%';
-            weather.textContent = data.list[i].weather[0].main 
+            // windSpeed.textContent = 'Wind: ' + data.list[i].wind.speed + ' MPH';
+            // humidity.textContent = 'Humidity: ' + data.list[i].main.humidity + '%';
+            // weather.textContent = data.list[i].weather[0].main 
 
-            // date.textContent = data.list[0].dt_txt;
-            // temperature.textContent = 'Temp: ' + data.list[0].main.temp + '° F';
-            // windSpeed.textContent = 'Wind: ' + data.list[0].wind.speed + ' MPH';
-            // humidity.textContent = 'Humidity: ' + data.list[0].main.humidity + '%';
-            // console.log(icon);
-
-            // if(icon === '04d') {
-            //     var icon = document.createElement('img');
-                    // currentWeather.append()
-            // }
-
-            //dont need city to show several times in loop
-            currentWeather.append(date);
-            currentWeather.append(temperature);
-            currentWeather.append(weather);
-            //currentWeather.append(icon);
-            currentWeather.append(windSpeed);
-            currentWeather.append(humidity);
+            // //dont need city to show several times in loop
+             weatherCardsDiv.append(date.textContent);
+            // currentWeather.append(temperature);
+            // currentWeather.append(weather);
+            // //currentWeather.append(icon);
+            // currentWeather.append(windSpeed);
+            // currentWeather.append(humidity);
             }
             })
        }
